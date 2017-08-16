1fa1534316546aecafc989604d498adf62748280
Jane <lajane2020@hotmail.com>
2017-08-15 23:50:57 -0500

    Cambios

--- resources/views/backEnd/orden/index.blade.php
+++ resources/views/backEnd/orden/index.blade.php
@@ -14,6 +14,7 @@ Orden
                 </tr>
             </thead>
             <tbody>
+            
             @foreach($ordenes as $item)
                 <tr>
                 	<td>{{ $item->id }}</td>
